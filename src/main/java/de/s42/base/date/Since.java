860f3cc67f495c6c633e2f5c43ce8be262aa24c4
Benjamin Schiller <bs@s42m.de>
2023-12-24 21:44:08 +0100

    First version of simple since display helper

---
+++ src/main/java/de/s42/base/date/Since.java
@@ -0,0 +1,89 @@
+// <editor-fold desc="The MIT License" defaultstate="collapsed">
+/*
+ * The MIT License
+ * 
+ * Copyright 2023 Studio 42 GmbH ( https://www.s42m.de ).
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+//</editor-fold>
+package de.s42.base.date;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Benjamin Schiller
+ */
+public class Since
+{
+
+	private final static int SECOND_MILLIS = 1000;
+	private final static int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+	private final static int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+	private final static int DAY_MILLIS = 24 * HOUR_MILLIS;
+	
+	private final static DateFormat DATE_FORMAT = new SimpleDateFormat("yyy-MM-dd HH:mm");
+	
+	public static String formatSince(Date date)
+	{
+		assert date != null;
+		
+		String timeAgo = getTimeAgo(date.getTime());
+		
+		String dateFormatted = DATE_FORMAT.format(date);
+		
+		return timeAgo + " (" + dateFormatted + ")";
+	}
+
+	public static String getTimeAgo(long since)
+	{
+
+		long now = System.currentTimeMillis();
+
+		if (since > now || since <= 0) {
+			return null;
+		}
+
+		// TODO: localize
+		final long diff = now - since;
+		if (diff < MINUTE_MILLIS) {
+			return "just now";
+		} else if (diff < 2 * MINUTE_MILLIS) {
+			return "a minute ago";
+		} else if (diff < 50 * MINUTE_MILLIS) {
+			return diff / MINUTE_MILLIS + " minutes ago";
+		} else if (diff < 90 * MINUTE_MILLIS) {
+			return "an hour ago";
+		} else if (diff < 24 * HOUR_MILLIS) {
+			return diff / HOUR_MILLIS + " hours ago";
+		} else if (diff < 48 * HOUR_MILLIS) {
+			return "yesterday";
+		} else {
+			return diff / DAY_MILLIS + " days ago";
+		}
+	}
+
+	// <editor-fold desc="Getters/Setters" defaultstate="collapsed">
+	//</editor-fold>
+	// <editor-fold desc="Hashcode/Equals/ToString/Compare" defaultstate="collapsed">
+	//</editor-fold>
+}
