051f75d0fd7d3e358cb1c68bde86633b5ef65081
Benjamin Schiller <bs@s42m.de>
2024-06-15 23:51:29 +0200

    Added MathHelper.pow10 flavors

--- src/main/java/de/s42/base/math/MathHelper.java
+++ src/main/java/de/s42/base/math/MathHelper.java
@@ -1,19 +1,19 @@
 // <editor-fold desc="The MIT License" defaultstate="collapsed">
 /*
  * The MIT License
- * 
+ *
  * Copyright 2024 Studio 42 GmbH ( https://www.s42m.de ).
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -36,9 +36,83 @@ public final class MathHelper
 
 	public final static double SQRT3 = Math.sqrt(3.0);
 
+	public final static double TAU = Math.PI * 2.0;
+
+	public static final long[] POWERS_OF_TEN_LONG = {
+		1L,
+		10L,
+		100L,
+		1_000L,
+		10_000L,
+		100_000L,
+		1_000_000L,
+		10_000_000L,
+		100_000_000L,
+		1_000_000_000L,
+		10_000_000_000L,
+		100_000_000_000L,
+		1_000_000_000_000L,
+		10_000_000_000_000L,
+		100_000_000_000_000L,
+		1_000_000_000_000_000L};
+
+	public static final double[] POWERS_OF_TEN_DOUBLE = {
+		1.0,
+		10.0,
+		100.0,
+		1_000.0,
+		10_000.0,
+		100_000.0,
+		1_000_000.0,
+		10_000_000.0,
+		100_000_000.0,
+		1_000_000_000.0,
+		10_000_000_000.0,
+		100_000_000_000.0,
+		1_000_000_000_000.0,
+		10_000_000_000_000.0,
+		100_000_000_000_000.0,
+		1_000_000_000_000_000.0};
+
 	private MathHelper()
 	{
 		// do nothing
+	}
+
+	public static final double pow10Double(int exp)
+	{
+		if (exp < 0 || exp > 15) {
+			throw new IllegalArgumentException("exp has to be 0 - 15");
+		}
+
+		return POWERS_OF_TEN_DOUBLE[exp];
+	}
+
+	public static final double pow10(double number, int exp)
+	{
+		if (exp < 0 || exp > 15) {
+			throw new IllegalArgumentException("exp has to be 0 - 15");
+		}
+
+		return number * POWERS_OF_TEN_DOUBLE[exp];
+	}
+
+	public static final long pow10Long(int exp)
+	{
+		if (exp < 0 || exp > 15) {
+			throw new IllegalArgumentException("exp has to be 0 - 15");
+		}
+
+		return POWERS_OF_TEN_LONG[exp];
+	}
+
+	public static final long pow10(long number, int exp)
+	{
+		if (exp < 0 || exp > 15) {
+			throw new IllegalArgumentException("exp has to be 0 - 15");
+		}
+
+		return number * POWERS_OF_TEN_LONG[exp];
 	}
 
 	public static final double saturate(double value)
