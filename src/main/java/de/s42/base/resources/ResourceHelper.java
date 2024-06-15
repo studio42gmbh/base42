c37ae91608ec66f21b85fadedf613838b664d938
Benjamin Schiller <bs@s42m.de>
2024-06-15 14:41:30 +0200

    Replaced string based charset lookup with StandardCharsets.UTF_8

--- src/main/java/de/s42/base/resources/ResourceHelper.java
+++ src/main/java/de/s42/base/resources/ResourceHelper.java
@@ -1,19 +1,19 @@
 // <editor-fold desc="The MIT License" defaultstate="collapsed">
 /*
  * The MIT License
- * 
+ *
  * Copyright 2022 Studio 42 GmbH ( https://www.s42m.de ).
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -137,7 +137,7 @@ public final class ResourceHelper
 		assert resourceName != null : "resourceName != null";
 		assert classLoader != null : "classLoader != null";
 
-		return new String(getZippedSingleFileResourceAsByteArray(resourceName, classLoader), "UTF-8");
+		return new String(getZippedSingleFileResourceAsByteArray(resourceName, classLoader), StandardCharsets.UTF_8);
 	}
 
 	public static byte[] getZippedSingleFileResourceAsByteArray(String resourceName) throws IOException
--- src/main/java/de/s42/base/web/WebPost.java
+++ src/main/java/de/s42/base/web/WebPost.java
@@ -1,19 +1,19 @@
 // <editor-fold desc="The MIT License" defaultstate="collapsed">
 /*
  * The MIT License
- * 
+ *
  * Copyright 2022 Studio 42 GmbH ( https://www.s42m.de ).
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -34,6 +34,7 @@ import java.net.CookieManager;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -133,7 +134,7 @@ public class WebPost
 			if (entry.getValue() instanceof byte[] bs) {
 				os.write(bs);
 			} else {
-				os.write(entry.getValue().toString().getBytes("UTF-8"));
+				os.write(entry.getValue().toString().getBytes(StandardCharsets.UTF_8));
 			}
 		}
 
@@ -164,7 +165,7 @@ public class WebPost
 		for (int length; (length = in.read(buffer)) != -1;) {
 			result.write(buffer, 0, length);
 		}
-		callResult = result.toString("UTF-8");
+		callResult = result.toString(StandardCharsets.UTF_8);
 
 		in.close();
 
